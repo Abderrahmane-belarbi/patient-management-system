ac786e5e003cd40bbe190a04e24803a6e49c9a1b
Abderrahmane Belarbi <abdoustart007@gmail.com>
2024-10-15 10:09:38 -0930

    adding files

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
       <section className="remove-scrollbar container my-auto">
-
+        <div className="sub-container max-w-[496px]">
+          <Image
+            alt='register-image'
+            src='/assets/images/register-img.png'
+            width={1000}
+            height={1000}
+          />
+        </div>
       </section>
     </div>
   );
